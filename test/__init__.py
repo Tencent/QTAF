24a85791e29bdf50a227c47ded84f4099f23f2f9
eeelin <eeelin@tencent.com>
2017-11-13 16:50:42 +0800

    fix CI conf

--- test/__init__.py
+++ test/__init__.py
@@ -11,3 +11,21 @@
 # OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 #
+
+
+import unittest
+import os
+import sys
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(test_dir))
+
+def main():
+    runner = unittest.TextTestRunner(verbosity=1 + sys.argv.count('-v'))
+    suite = unittest.TestLoader().discover(test_dir,  pattern='*test.py')
+    raise SystemExit(not runner.run(suite).wasSuccessful())
+
+
+if __name__ == '__main__':
+    main()
+    
