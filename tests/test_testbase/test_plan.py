4fc60caa3d3d336b9d9a791adc4e9e068f18274d
eeelin <eeelin@tencent.com>
2019-07-22 13:31:23 +0800

    fix TestPlan debug_run error

---
+++ tests/test_testbase/test_plan.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+
+import unittest
+from testbase.report import StreamTestReport
+
+class PlanTest(unittest.TestCase):
+
+    def test_debug_run(self):
+        from tests.sampletestplan.hello import HelloTestPlan
+        report = HelloTestPlan().debug_run()
+        self.assertIsInstance(report, StreamTestReport)
+
+
