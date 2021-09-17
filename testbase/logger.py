99dbea1f9f14b87f7a46a2e18863c4d0addce71d
vanzhang <vanzhang@futunn.com>
2021-09-17 18:04:08 +0800

    update: 调整报告结构，支持同时输入console和html报告

--- testbase/logger.py
+++ testbase/logger.py
@@ -2,12 +2,12 @@
 #
 # Tencent is pleased to support the open source community by making QTA available.
 # Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
-# Licensed under the BSD 3-Clause License (the "License"); you may not use this 
+# Licensed under the BSD 3-Clause License (the "License"); you may not use this
 # file except in compliance with the License. You may obtain a copy of the License at
-# 
+#
 # https://opensource.org/licenses/BSD-3-Clause
-# 
-# Unless required by applicable law or agreed to in writing, software distributed 
+#
+# Unless required by applicable law or agreed to in writing, software distributed
 # under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS
 # OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
@@ -18,19 +18,23 @@
 import logging
 import sys
 import traceback
+
+import Testcase.settings as settings
 from testbase import context
 from testbase.util import ensure_binary_stream, smart_binary
-    
+
 _stream, _encoding = ensure_binary_stream(sys.stdout)
 
 class _Formatter(logging.Formatter):
     def format(self, record):
-        s = super(_Formatter, self).format(record)
+        _f = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
+        s = _f.format(record)
         return smart_binary(s, encoding=_encoding)
-    
+
 _stream_handler=logging.StreamHandler(_stream)
 _stream_handler.terminator = b"\n"
 _stream_handler.setFormatter(_Formatter())
+_stream_handler.setLevel(logging.DEBUG)
 
 class TestResultBridge(logging.Handler):
     '''中转log信息到TestResult
@@ -40,23 +44,27 @@ class TestResultBridge(logging.Handler):
         '''
         testresult = context.current_testresult()
         if testresult is None:
-            _stream_handler.emit(log_record)
+            # _stream_handler.emit(log_record)
             return
         record = {}
         if log_record.exc_info:
             record['traceback'] = ''.join(traceback.format_tb(log_record.exc_info[2])) + '%s: %s' %(
                                    log_record.exc_info[0].__name__, log_record.exc_info[1])
         testresult.log_record(log_record.levelno, log_record.msg, record)
-        
+
 _LOGGER_NAME = "QTA_LOGGER"
 _logger = logging.getLogger(_LOGGER_NAME)
+result_handler = TestResultBridge()
+result_handler.setLevel(logging.DEBUG if settings.DEBUG else logging.INFO)
 _logger.setLevel(logging.DEBUG)
-_logger.addHandler(TestResultBridge())
+if settings.CONSOLE_REPORT:
+    _logger.addHandler(_stream_handler)
+_logger.addHandler(result_handler)
 
-        
+
 def critical(msg, *args, **kwargs):
     _logger.error(msg, *args, **kwargs)
- 
+
 fatal = critical
 
 def error(msg, *args, **kwargs):
@@ -90,12 +98,12 @@ def log(level, msg, *args, **kwargs):
     '''Log 'msg % args' with the integer severity 'level' on the root logger.
     '''
     _logger.log(level, msg, *args, **kwargs)
-    
+
 def addHandler(hdlr):
     '''Add the specified handler to this logger.
     '''
     _logger.addHandler(hdlr)
-    
+
 def removeHandler(hdlr):
     '''Remove the specified handler from this logger.
     '''
