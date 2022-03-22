c545ee987112d2f476780e57dea53815e0f2c9f7
Ming5024 <774517491@qq.com>
2022-03-22 17:45:31 +0800

    fix get extra properties error

--- testbase/util.py
+++ testbase/util.py
@@ -31,6 +31,7 @@ import threading
 import time
 import traceback
 
+from inspect import isclass, getmro, types
 from xml.dom.minidom import Node
 from datetime import datetime
 
@@ -682,6 +683,53 @@ def get_attribute_from_string(object_path):
         except AttributeError:
             raise AttributeError("%s has no attribute or submodule named \"%s\"" % (value, parts[new_index]))
     return value
+
+
+def getmembers(object, predicate=None):
+    """Return all members of an object as (name, value) pairs sorted by name.
+    Optionally, only return members that satisfy a given predicate."""
+    if isclass(object):
+        mro = (object,) + getmro(object)
+    else:
+        mro = ()
+    results = []
+    processed = set()
+    names = dir(object)
+    # :dd any DynamicClassAttributes to the list of names if object is a class;
+    # this may result in duplicate entries if, for example, a virtual
+    # attribute with the same name as a DynamicClassAttribute exists
+    try:
+        for base in object.__bases__:
+            for k, v in base.__dict__.items():
+                if isinstance(v, types.DynamicClassAttribute):
+                    names.append(k)
+    except:
+        pass
+    for key in names:
+        # First try to get the value via getattr.  Some descriptors don't
+        # like calling their __get__ (see bug #1785), so fall back to
+        # looking in the __dict__.
+        try:
+            value = getattr(object, key)
+            # handle the duplicate key
+            if key in processed:
+                raise AttributeError
+        except AttributeError:
+            for base in mro:
+                if key in base.__dict__:
+                    value = base.__dict__[key]
+                    break
+            else:
+                # could be a (currently) missing slot member, or a buggy
+                # __dir__; discard and move on
+                continue
+        except:
+            continue
+        if not predicate or predicate(value):
+            results.append((key, value))
+        processed.add(key)
+    results.sort(key=lambda pair: pair[0])
+    return results
 
 
 if __name__ == "__main__":
