4d5d52e35a62e8c56926dbc1f0e5d5fb7d6fe519
alexwjxie <alexwjxie@tencent.com>
2022-10-27 11:31:10 +0800

    update xmlResult

--- testbase/testresult.py
+++ testbase/testresult.py
@@ -572,8 +572,8 @@ class XmlResult(TestResultBase):
         self._testnode.setAttribute('result', test_result)
         self._testnode.setAttribute('endtime', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(self.end_time)))
         self._testnode.setAttribute('duration', "%02d:%02d:%02.2f\n" % _convert_timelength(self.end_time - self.begin_time))
-        if self.__last_failed_stage:
-            self._testnode.setAttribute('last_failed_stage', self.__last_failed_stage)
+        if self.last_failed_stage:
+            self._testnode.setAttribute('last_failed_stage', self.last_failed_stage)
         if self._file_path:
             with codecs_open(smart_text(self._file_path), 'wb') as fd:
                 fd.write(to_pretty_xml(self._xmldoc))
